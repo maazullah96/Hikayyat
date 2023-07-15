a9b85d69ac4a5126d21c2a9fa28823a61785bd8a
Maaz Ullah <maazullahminhas96@gmail.com>
2023-07-15 15:58:10 +0500

    Update config.js

--- backend/src/config.js
+++ backend/src/config.js
@@ -3,7 +3,7 @@
 (PORT = process.env.PORT || 5000), (NODE_ENV = "development");
 MONGO_URI =
   process.env.MONGO_URI ||
-  "mongodb+srv://maazgeass:Overhaul_1996@deploy1-6djhu.mongodb.net/test?retryWrites=true";
+  
 SESS_NAME = "sid";
 SESS_SECRET = "secret!session";
 SESS_LIFETIME = 1000 * 60 * 60 * 2;
